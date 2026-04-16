ce5b9647d0fbf13dd1356b030216c6ec5571a30e
Afilaga <andreyfilatiev@gmail.com>
2026-04-16 15:46:01 +0300

    feat: move yandex metrica and json-ld to head and optimize strategy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -261,12 +261,12 @@ export default function RootLayout({
       <body
         className={`${oswald.variable} ${jetbrainsMono.variable} antialiased bg-black text-white font-sans selection:bg-white selection:text-black`}
       >
-        <Script id="local-business-jsonld" type="application/ld+json" strategy="afterInteractive">
+        <Script id="local-business-jsonld" type="application/ld+json" strategy="beforeInteractive">
           {JSON.stringify(localBusinessJsonLd)}
         </Script>
 
         {/* Yandex.Metrika counter */}
-        <Script id="yandex-metrika" strategy="afterInteractive">
+        <Script id="yandex-metrika" strategy="beforeInteractive">
           {`
             (function(m,e,t,r,i,k,a){
                 m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
@@ -275,15 +275,17 @@ export default function RootLayout({
                 k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
             })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${YANDEX_METRIKA_ID}', 'ym');
 
-            ym(${YANDEX_METRIKA_ID}, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+            ym(${YANDEX_METRIKA_ID}, 'init', {webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
           `}
         </Script>
+
         <noscript>
           <div>
             <img src={`https://mc.yandex.ru/watch/${YANDEX_METRIKA_ID}`} style={{ position: 'absolute', left: '-9999px' }} alt="" />
           </div>
         </noscript>
         {/* /Yandex.Metrika counter */}
+
 
         {children}
         <Script src="https://dikidi.net/assets/js/widget_record/widget2.min.js?v=1773811740" strategy="lazyOnload" />
